433afdd458185886ed312d005aa662297990fadf
Batyrkhan Shakenov <batr1996@gmail.com>
2024-12-19 15:36:59 +0500

    add carting system and location map

--- coffee/models.py
+++ coffee/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Coffee(models.Model):
     name = models.CharField(max_length=255)
     price = models.FloatField()
     quantity = models.IntegerField()
     image = models.CharField(max_length=2083)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    session_key = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
+    coffee = models.ForeignKey('Coffee', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def total_price(self):
+        return self.quantity * self.coffee.price
--- coffee/urls.py
+++ coffee/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('location/', views.location, name='location'),
+    path('add-to-cart/<int:coffee_id>/', views.add_to_cart, name='add_to_cart'),
+    path('api/cart/remove/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
+    path('api/cart/', views.get_cart_items, name='get_cart_items'),
 ]
